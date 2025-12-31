45727952507943fad89a2c00b816d3bd63bab24c
adrmitbro <adrian.mitchellbrown@gmail.com>
2025-12-31 11:05:19 -0700

    Update server.js

--- server.js
+++ server.js
@@ -625,14 +625,13 @@ marker.getPopup().setContent(popupContent);
             }).addTo(map);
 
 const popupContent = \`
-    <div style="min-width:200px">
-        <h4 style="margin:0 0 5px 0">${ac.atcId}${ac.isPaused ? ' <span style="color:#ff0000;font-style:italic;">(PAUSED)</span>' : ''}</h4>
-        <p style="margin:0 0 3px 0; font-size:10px; color:#888;">User ID: ${ac.uniqueId}</p>
-        <p style="margin:0 0 5px 0">Aircraft: ${ac.atcModel}</p>
-        <p style="margin:0 0 5px 0">Speed: ${Math.round(ac.groundSpeed)} kts</p>
-        <p style="margin:0 0 5px 0">Altitude: ${Math.round(ac.altitude)} ft</p>
-        <p style="margin:0">Heading: ${Math.round(ac.heading)}°</p>
-    </div>
+                <div style="min-width:200px">
+                    <h4 style="margin:0 0 5px 0">\${ac.atcId}\${ac.isPaused ? ' <span style="color:#ff0000;font-style:italic;">(PAUSED)</span>' : ''}</h4>
+                    <p style="margin:0 0 5px 0">Aircraft: \${ac.atcModel}</p>
+                    <p style="margin:0 0 5px 0">Speed: \${Math.round(ac.groundSpeed)} kts</p>
+                    <p style="margin:0 0 5px 0">Altitude: \${Math.round(ac.altitude)} ft</p>
+                    <p style="margin:0">Heading: \${Math.round(ac.heading)}°</p>
+                </div>
             \`;
 
             marker.bindPopup(popupContent, {
