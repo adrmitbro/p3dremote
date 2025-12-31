6ff2d10f7cfc158818864feec5dd609912dfa835
adrmitbro <adrian.mitchellbrown@gmail.com>
2025-12-31 11:21:35 -0700

    Update server.js

--- server.js
+++ server.js
@@ -611,6 +611,7 @@ if (!lastPos || lastPos[0] !== ac.latitude || lastPos[1] !== ac.longitude) {
 const popupContent = \`
     <div style="min-width:200px">
         <h4 style="margin:0 0 5px 0">\${ac.atcId}\${ac.isPaused ? ' <span style="color:#ff0000;font-style:italic;">(PAUSED)</span>' : ''}</h4>
+        <p style="margin:0 0 3px 0; font-size:10px; color:#888;">User ID: ${ac.uniqueId}</p>
         <p style="margin:0 0 5px 0">Aircraft: \${ac.atcModel}</p>
         <p style="margin:0 0 5px 0">Speed: \${Math.round(ac.groundSpeed)} kts</p>
         <p style="margin:0 0 5px 0">Altitude: \${Math.round(ac.altitude)} ft</p>
@@ -3767,6 +3768,7 @@ window.onload = () => {
 server.listen(PORT, () => {
   console.log(`P3D Remote Cloud Relay running on port ${PORT}`);
 });
+
 
 
 
