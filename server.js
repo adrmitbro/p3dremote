3386853709c63b0d77b7ad93951be16eb50251df
adrmitbro <adrian.mitchellbrown@gmail.com>
2025-12-31 18:13:17 -0700

    Update server.js

--- server.js
+++ server.js
@@ -614,12 +614,13 @@ const isSelected = selectedAircraftId === uniqueId;
 // Update popup content without closing it
 const popupContent = \`
     <div style="min-width:180px">
-        <h4 style="margin:0 0 5px 0">${ac.atcId}${ac.isPaused ? ' <span style="color:#ff0000;font-style:italic;">(PAUSED)</span>' : ''}</h4>
+        <h4 style="margin:0 0 5px 0">\${ac.atcId}\${ac.isPaused ? ' <span style="color:#ff0000;font-style:italic;">(PAUSED)</span>' : ''}</h4>
         <p style="margin:0 0 2px 0;font-size:10px;font-style:italic;color:#888;">${ac.uniqueId}</p>
-        <p style="margin:0 0 5px 0">Aircraft: ${ac.atcModel}</p>
-        <p style="margin:0 0 5px 0">Speed: ${Math.round(ac.groundSpeed)} kts</p>
-        <p style="margin:0 0 5px 0">Altitude: ${Math.round(ac.altitude)} ft</p>
-        <p style="margin:0">Heading: ${Math.round(ac.heading)}°</p>
+        <p style="margin:0 0 5px 0">Aircraft: \${ac.atcModel}</p>
+        <p style="margin:0 0 5px 0">Speed: \${Math.round(ac.groundSpeed)} kts</p>
+        <p style="margin:0 0 5px 0">Altitude: \${Math.round(ac.altitude)} ft</p>
+        <p style="margin:0">Heading: \${Math.round(ac.heading)}°</p>
+
     </div>
 \`;
 marker.getPopup().setContent(popupContent);
@@ -3828,8 +3829,6 @@ window.onload = () => {
 server.listen(PORT, () => {
   console.log(`P3D Remote Cloud Relay running on port ${PORT}`);
 });
-
-
 
 
 
